20e6e65fbf3a56ee8f7f7ec64a3242fe77cd7b53
Davit Soitashvili <davit.soitashvili@space.ge>
2021-05-28 13:19:10 +0400

    update unit test

--- app/src/test/java/com/example/testingapplication/CalculatorTest.kt
+++ app/src/test/java/com/example/testingapplication/CalculatorTest.kt
@@ -1,21 +1,30 @@
 package com.example.testingapplication
 
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
 import org.junit.Test
 
 class CalculatorTest {
 
     @Test
     fun `getCalculatedSum returns valid value`() {
+        // arrange
         val expectedResult = 30
         val calculator = Calculator()
-        assert(calculator.getCalculatedSum(10, 20) == expectedResult)
+        // act
+        val result = calculator.getCalculatedSum(10, 20)
+        // assert
+        assertEquals(result, expectedResult)
     }
 
     @Test
     fun `getCalculatedSum returns invalid value`() {
+        // arrange
         val expectedResult = 30
         val calculator = Calculator()
-        assert(calculator.getCalculatedSum(10, 22) != expectedResult)
+        // act
+        val result = calculator.getCalculatedSum(10, 22)
+        // assert
+        assertNotEquals(result, expectedResult)
     }
-
 }
